5aeccced4f8e475a74c3c4300f13fd84a0b6c246
Ubuntu <ubuntu@ip-172-31-26-27.us-west-1.compute.internal>
2017-05-02 21:46:32 +0000

    got oath working

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502182017) do
+ActiveRecord::Schema.define(version: 20170502195100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,20 @@ ActiveRecord::Schema.define(version: 20170502182017) do
     t.string "text"
     t.string "authors"
     t.string "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
+    t.string   "name"
+    t.string   "location"
+    t.string   "image_url"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
+    t.index ["provider"], name: "index_users_on_provider", using: :btree
+    t.index ["uid"], name: "index_users_on_uid", using: :btree
   end
 
 end

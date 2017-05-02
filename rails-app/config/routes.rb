66e310282937da5179cf93433136b11c01ee129b
Ubuntu <ubuntu@ip-172-31-26-27.us-west-1.compute.internal>
2017-05-02 22:10:18 +0000

    routes

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  post '/action', to: "main#action"
   root to: 'main#index'
 end

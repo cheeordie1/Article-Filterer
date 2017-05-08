0065ec7824266c0dbf87b091a6b03165d896445e
Joey Asperger <joeyasperger@gmail.com>
2017-05-08 18:19:12 +0000

    check for duplicate articles before adding to database

--- rails-app/app/controllers/main_controller.rb
+++ rails-app/app/controllers/main_controller.rb
@@ -15,29 +15,34 @@ class MainController < ApplicationController
 
     def action
         if params['read']
-            url = params['url']
-            cmd = "python3 app/scripts/get_article.py %s" % Shellwords.escape(url)
-            out, err, st = Open3.capture3(cmd)
-            artjson = JSON.parse(out)
-            article = Article.new()
-            article[:title] = artjson["title"]
-            article[:text] = artjson["text"]
-            article[:url] = url
-            article.save
-            UserArticle.create(:user => current_user, :article => article)
+            article = addArticleForUser(params['url'], current_user)         
             redirect_to root_url(:article => article.id)
         else
-            url = params['url']
-            cmd = "python3 app/scripts/get_article.py %s" % Shellwords.escape(url)
-            out, err, st = Open3.capture3(cmd)
-            artjson = JSON.parse(out)
-            article = Article.new()
-            article[:title] = artjson["title"]
-            article[:text] = artjson["text"]
-            article[:url] = url
-            article.save
-            UserArticle.create(:user => current_user, :article => article)
+            addArticleForUser(params['url'], current_user)
             redirect_to root_url(:result => "added")
         end  
     end
+
+    private
+
+    def addArticleForUser(url, user)
+        article = Article.find_by_url(url)
+        if article == nil
+            cmd = "python3 app/scripts/get_article.py %s" % Shellwords.escape(url)
+            out, err, st = Open3.capture3(cmd)
+            artjson = JSON.parse(out)
+            article = Article.new()
+            article[:title] = artjson["title"]
+            article[:text] = artjson["text"]
+            article[:url] = url
+            article[:authors] = JSON.dump(artjson["authors"])
+            article.save
+        end
+        if UserArticle.where(user: user, article: article).length == 0
+            UserArticle.create(user: user, article: article)
+        end
+        return article
+    end
+
+
 end

a2e1eac83969418ffbab5a22eb5d300506568641
Nicholas Chee-Awai <cheeordie1@gmail.com>
2017-05-18 19:09:57 -0700

    added some broken article display

--- rails-app/app/models/article.rb
+++ rails-app/app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ActiveRecord::Base
     has_many :user_articles
     has_many :users, :through => :user_articles
+
+    def getTitle
+      return self.title
+    end
+
+    def getParagraphs
+      return self.text
+    end
+
 end
